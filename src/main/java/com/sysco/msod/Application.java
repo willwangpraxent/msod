b2b7efbbf61bfb4207a395107221b18444b6b2cb
willwangpraxent <streetpoet@163.com>
2017-01-31 15:50:07 -0600

    add docker file, bug fixing

--- src/main/java/com/sysco/msod/Application.java
+++ src/main/java/com/sysco/msod/Application.java
@@ -1,9 +1,8 @@
 package com.sysco.msod;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +16,7 @@ public class Application {
     }
 	
     @RequestMapping("/{username}")
-    public String homeWithName(@PathParam("username") String username) {
+    public String homeWithName(@PathVariable("username") String username) {
         return String.format("Welcome, %s", username == null ? "Everyone" : username);
     }
 
