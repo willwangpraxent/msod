c82c19c23aad2fb36929c9c3e6c0c43fcfc41c6d
willwangpraxent <streetpoet@163.com>
2017-01-31 14:31:24 -0600

    add docker plugin, add demo classes & test, add gradle wrapper

---
+++ src/main/java/com/sysco/msod/Application.java
@@ -0,0 +1,28 @@
+package com.sysco.msod;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@RequestMapping("/")
+    public String home() {
+        return homeWithName(null);
+    }
+	
+    @RequestMapping("/{username}")
+    public String homeWithName(@PathParam("username") String username) {
+        return String.format("Welcome, %s", username == null ? "Everyone" : username);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sysco/msod/ApplicationTest.java
@@ -0,0 +1,35 @@
+package com.sysco.msod;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DirtiesContext
+public class ApplicationTest {
+
+	@LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testHome() throws Exception {
+        ResponseEntity<String> entity = restTemplate
+                .getForEntity("http://localhost:" + this.port + "/", String.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+    }
+
+}
